3ecba7dcad54fc780860f68648763ff55d9de799
한승현 <vkflek1232@naver.com>
2023-06-05 20:36:50 +0900

    기초 css + 홈화면css

--- hands/src/components/Home/Home.jsx
+++ hands/src/components/Home/Home.jsx
@@ -1,21 +1,65 @@
-import React, { useEffect } from 'react'
-import { useDispatch, useSelector } from "react-redux";
-import { addUser} from "../../store/action/userAction.js"
+import React from 'react'
+// import { useDispatch, useSelector } from "react-redux";
+// import { addUser } from "../../store/action/userAction.js"
+import { styled } from "styled-components";
 const Home = () => {
-  const dispatch = useDispatch();
-  const user = useSelector((store) => store.userReduer);
-  useEffect(() => {
-    console.log(user);
-  }, [user]);
+  // const user = useSelector((store) => store.userReduer);
+  // useEffect(() => {
+  //   console.log(user);
+  // }, [user]);
   return (
-    <div>
-      <div>{ user.name}</div>
-      <button onClick={() => {
-        dispatch(
-          addUser({ name: "한승현" })
-      )}}>버튼</button>
-    </div>
+    <Main className="container">
+      <Btn>
+        <img src="assets/image/volunteer_activism_FILL0_wght200_GRAD200_opsz48.svg" width="40%" height="100%" alt="" />
+        <BtnText>도움이<br/>필요해요</BtnText>
+      </Btn>
+      <Btn>
+        <img src="assets/image/handshake_FILL0_wght200_GRAD200_opsz48.svg" width="40%" height="100%" alt="" />
+        <BtnText>도움을<br/>줄래요</BtnText>
+      </Btn>
+    </Main>
   )
 }
+const Btn = styled.button`
+  width: 80%;
+  height: 20%;
+  padding: 2rem;
+  display: flex;
+  box-sizing: border-box;
+`
+const BtnText = styled.div`
+  width: 70%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 1.5rem;
+  white-space: nowrap;
+`
+const Main = styled.div`
+  width:100%;
+  height:100%;
+  padding-top: 5rem;
+
+  position: absolute;
+  top: 0;
+
+  background-image: url("assets/image/Men and women welcoming people with disabilities.jpg");
+  background-size: 30%;
+  @media screen and (max-width: 1024px){
+    background-size: 60%;
+  }
+  @media screen and (max-width: 768px){
+    background-size: 90%;
+  }
+  background-repeat: no-repeat;
+  background-position: bottom;
+
+  display: flex;
+  box-sizing: border-box;
+  align-items: center;
+  flex-direction: column;
+  gap: 5rem;
+`
 
 export default Home
---
+++ readme.md
@@ -0,0 +1,4 @@
+# 우리손을 잡아
+
+- 출처
+  홈화면 일러스트 : <a href="https://kr.freepik.com/free-vector/men-and-women-welcoming-people-with-disabilities-group-of-people-meeting-blind-female-character-and-male-in-wheelchair_16375447.htm#query=%EC%9E%A5%EC%95%A0&position=0&from_view=keyword&track=sph">작가 pch.vector</a> 출처 Freepik
